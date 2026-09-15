80c8c742ca95105cbe4cfb0890aa8c34bf226427
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-15 10:12:26 +0500

    refactor: simplify user mapper

--- src/main/java/com/taskmanager/taskmanager/mapper/UserMapper.java
+++ src/main/java/com/taskmanager/taskmanager/mapper/UserMapper.java
@@ -1,16 +1,13 @@
 package com.taskmanager.taskmanager.mapper;
 
 import com.taskmanager.taskmanager.dto.UserDTO;
-
 import com.taskmanager.taskmanager.entity.User;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
     UserDTO toDTO(User user);
+
     User toEntity(UserDTO userDTO);
-    List<UserDTO> USER_DTO_LIST(List<User> users);
-    List<User> USERS(List<UserDTO> userDTOS);
 }
