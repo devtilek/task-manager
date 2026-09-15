390c0ab096a7a6e78ee0bde1147ec85e03166ae9
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-15 10:13:09 +0500

    refactor: implement task rest crud

--- src/main/java/com/taskmanager/taskmanager/controller/TaskController.java
+++ src/main/java/com/taskmanager/taskmanager/controller/TaskController.java
@@ -2,30 +2,45 @@ package com.taskmanager.taskmanager.controller;
 
 import com.taskmanager.taskmanager.dto.TaskDTO;
 import com.taskmanager.taskmanager.service.TaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/tasks")
 public class TaskController {
 
     private final TaskService taskService;
 
+    @GetMapping
+    public List<TaskDTO> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
     @GetMapping("/{id}")
-    public TaskDTO getTaskById(@PathVariable UUID id){
+    public TaskDTO getTaskById(@PathVariable UUID id) {
         return taskService.getTaskById(id);
     }
 
     @PostMapping
-    public void addUser(@RequestBody TaskDTO taskDTO){
-        taskService.addTask(taskDTO);
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDTO addTask(@Valid @RequestBody TaskDTO taskDTO) {
+        return taskService.addTask(taskDTO);
+    }
+
+    @PutMapping("/{id}")
+    public TaskDTO updateTask(@PathVariable UUID id, @Valid @RequestBody TaskDTO taskDTO) {
+        return taskService.updateTask(id, taskDTO);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable UUID id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable UUID id) {
         taskService.deleteById(id);
     }
 }
