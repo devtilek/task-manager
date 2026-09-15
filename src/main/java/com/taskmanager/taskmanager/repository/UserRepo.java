1c75984913a9f7014747faf6667446e0054df6c0
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-15 10:13:48 +0500

    refactor: simplify user repository

--- src/main/java/com/taskmanager/taskmanager/repository/UserRepo.java
+++ src/main/java/com/taskmanager/taskmanager/repository/UserRepo.java
@@ -2,12 +2,8 @@ package com.taskmanager.taskmanager.repository;
 
 import com.taskmanager.taskmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-@Repository
-@Transactional
-public interface UserRepo extends JpaRepository<User, UUID>{
+public interface UserRepo extends JpaRepository<User, UUID> {
 }
