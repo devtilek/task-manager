4e5b0b68ac0f00fba5e0ad3c17546ef8f5e9e9b6
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-15 10:12:23 +0500

    refactor: improve task mapper

--- src/main/java/com/taskmanager/taskmanager/mapper/TaskMapper.java
+++ src/main/java/com/taskmanager/taskmanager/mapper/TaskMapper.java
@@ -3,9 +3,14 @@ package com.taskmanager.taskmanager.mapper;
 import com.taskmanager.taskmanager.dto.TaskDTO;
 import com.taskmanager.taskmanager.entity.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
-    TaskDTO toDTO(Task task);
-    Task toEntity(TaskDTO taskDTO);
 
+    @Mapping(source = "user.id", target = "userId")
+    TaskDTO toDTO(Task task);
+
+    @Mapping(target = "user", ignore = true)
+    Task toEntity(TaskDTO taskDTO);
 }
