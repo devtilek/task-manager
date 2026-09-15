816b0fd498c2b3465fd5d1acfdc04e5e1028c408
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-15 10:13:44 +0500

    refactor: simplify task repository

--- src/main/java/com/taskmanager/taskmanager/repository/TaskRepo.java
+++ src/main/java/com/taskmanager/taskmanager/repository/TaskRepo.java
@@ -2,12 +2,8 @@ package com.taskmanager.taskmanager.repository;
 
 import com.taskmanager.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-@Repository
-@Transactional
 public interface TaskRepo extends JpaRepository<Task, UUID> {
 }
